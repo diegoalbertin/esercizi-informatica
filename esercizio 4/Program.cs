23af4e60cba6d18753d11dfe21ff2a51c22c79fc
diegoalbertin <72445590+diegoalbertin@users.noreply.github.com>
2021-10-17 14:46:19 +0200

    .

--- esercizio 4/Program.cs
+++ esercizio 4/Program.cs
@@ -10,27 +10,32 @@ namespace esercizio_4
        
 
         double r;
+        //costruttore
         public circonferenza()
         {
             r = 0;
         }
+        //metodo per l'insrimento del raggio
         public void inserisciRaggio()
         {
             Console.WriteLine("inserisci il raggio");
             r = Convert.ToDouble(Console.ReadLine());
         }
+        //metodo per il calcolo del diametro
         public void calcolaDiametro()
         {
             double diametro;
             diametro = r * 2;
             Console.WriteLine("il diametro è:" + diametro);
         }
+        //metodo per il calcolo della circonferenza
         public void calcolaCirconferenza()
         {
             double circonferenza;
             circonferenza = r * 2 * Math.PI;
             Console.WriteLine("la circonferenza è:" + circonferenza);
         }
+        //metodo per il calcolo dell'area
         public void calcolaArea()
         {
             double area;
@@ -39,7 +44,9 @@ namespace esercizio_4
         }
         static void Main(string[] args)
         {
+            //viene creata una nuova istanza della classe circonferenza
             circonferenza c = new circonferenza();
+            //vengono richiamati i metodi della classe circonferenza
             c.inserisciRaggio();
             c.calcolaDiametro();
             c.calcolaCirconferenza();
